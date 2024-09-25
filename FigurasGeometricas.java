4041441b736552ac24e259d4ac200f4dc21d59c0
Oscar Bautista <odbautista@profesores.uniajc.edu.co>
2024-09-24 19:27:06 -0500

    Prueba figuras geométricas

--- FigurasGeometricas.java
+++ FigurasGeometricas.java
@@ -14,37 +14,29 @@ public class FigurasGeometricas {
         this.opcionFiguraGeometrica = opcionFiguraGeometrica;
 
     }
-        public void calcularArea(){
-            switch (opcionFiguraGeometrica) {
-                case 1:
-                    float areaCuadrado = ladoCuadrado*ladoCuadrado;
-                    System.out.println("El área del cuadrado es: "+areaCuadrado);
-                    break;
-                
-                case 2:
-                    float areaTriangulo = ladoCuadrado*ladoCuadrado;
-                    System.out.println("El área del cuadrado es: "+areaTriangulo);   
-                    break;
-                
-                case 3:
-                    double areaCirculo = (Math.PI) *  radioCirculo*radioCirculo;
-                    System.out.println("El área del cuadrado es: "+areaCirculo);
-                    break;
-            
-                default:
-                    break;
-            }
 
+    public void calcularArea() {
+        switch (opcionFiguraGeometrica) {
+            case 1:
+                float areaCuadrado = ladoCuadrado * ladoCuadrado;
+                System.out.println("El área del cuadrado es: " + areaCuadrado);
+                break;
 
+            case 2:
+                float areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+                System.out.println("El área del cuadrado es: " + areaTriangulo);
+                break;
 
+            case 3:
+                double areaCirculo = (Math.PI) * radioCirculo * radioCirculo;
+                System.out.println("El área del cuadrado es: " + areaCirculo);
+                break;
 
-        
-   
-
-
+            default:
+                System.out.println("Opción no válida.");
+                break;
+        }
 
     }
 
-    
-    
 }
